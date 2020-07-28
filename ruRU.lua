15b39f0b9ebe2552682148e88b754c146dc61a5b
Hubbotu <34053496+Hubbotu@users.noreply.github.com>
2020-07-28 08:42:28 +0300

    Add files via upload

---
+++ ruRU.lua
@@ -0,0 +1,30 @@
+-- ruRU Russian
+
+lang = lang or {}
+
+function THLang_ruRU()
+	lang.darkmode = "Темный Режим"
+
+	lang.unitframes = "Рамка персонажа"
+
+	lang.changeplayercolorsonly = "Изменить только цвета игрока"
+	lang.changeplayerportraitsonly = "Изменить только портреты игрока"
+
+	lang.minimap = "Миникарта"
+	lang.minimapscale = "Масштаб миникарты: ЗНАЧЕНИЕ"
+	lang.minimapalpha = "Прозрачность миникарты: ЗНАЧЕНИЕ"
+	lang.minimapsquare = "Миникарта Квадратная"
+	lang.minimapfade = "Миникарта исчезает"
+
+	lang.talking = "Говорящая рамка"
+	
+	lang.freespace = "Свободное место в сумке"
+
+	lang.actionbuttonszoom = "Увеличить кнопку действия"
+
+	lang.hidebackgrounds = "Скрыть фон"
+	lang.hidebackgroundscaps = "Скрыть грифонов"
+	lang.hidebackgroundsactionbuttons = "Скрыть фон панели действий"
+	lang.hidebackgroundsstatusbar = "Скрыть наложение строки состояния"
+	lang.hidebackgroundsmicroandbags = "Скрыть фон сумок и микро кнопок"
+end
